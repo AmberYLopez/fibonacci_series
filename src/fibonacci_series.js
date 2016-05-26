f2add608cd682b5a1dddf3d46bd6d4b18cbfda31
AmberYLopez <1163281758@qq.com>
2016-05-26 13:12:19 +0800

    fibonacci_series

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+	var arr = [0,1];
+	var res = [];
+	
+	if(n < 2){
+		for(var i = 0; i <= n; i++){
+			res.push(arr[i]);
+			}	
+		}
+	else{
+		for(var j = 2; j <= n; j++){
+				arr[j] = arr[j-2]+arr[j-1];
+				}
+				res = arr;
+		}
+	return res;
 }
+
 
 module.exports = fibonacci_series;
