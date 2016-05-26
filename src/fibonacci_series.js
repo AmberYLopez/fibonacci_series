8dd04a08e509a4eb1e0c4bc5a7a2424fccb67eac
AmberYLopez <1163281758@qq.com>
2016-05-26 13:15:18 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -7,14 +7,14 @@ function fibonacci_series(n) {
 	if(n < 2){
 		for(var i = 0; i <= n; i++){
 			res.push(arr[i]);
-			}	
-		}
+		}	
+	}
 	else{
 		for(var j = 2; j <= n; j++){
-				arr[j] = arr[j-2]+arr[j-1];
-				}
-				res = arr;
+			arr[j] = arr[j-2]+arr[j-1];
 		}
+		res = arr;
+	}
 	return res;
 }
 
